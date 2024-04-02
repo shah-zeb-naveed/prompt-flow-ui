52e2af85721330c73bf506d2e385c78a11663fac
shah-zeb-naveed <shah_zeb_naveed@yahoo.com>
2024-04-03 01:43:42 +0500

    delete redundant code

--- app.py
+++ app.py
@@ -7,12 +7,7 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-AZURE_KEY = os.environ['AZURE_ENDPOINT']
-
-# #for proxy_env in ['http_proxy', 'https_proxy']
-# os.environ['https_proxy'] = ''
-# os.environ['http_proxy'] = ''
-
+AZURE_ENDPOINT_KEY = os.environ['AZURE_ENDPOINT_KEY']
 
 def allowSelfSignedHttps(allowed):
     # bypass the server certificate verification on the client side
@@ -20,15 +15,15 @@ def allowSelfSignedHttps(allowed):
         ssl._create_default_https_context = ssl._create_unverified_context
 
 def main():
-    st.title("Azure Prompt Flow Chat Interface")
+    
 
-    allowSelfSignedHttps(True)  # this line is needed if you use a self-signed certificate in your scoring service.
+    allowSelfSignedHttps(True)
+
+    st.title("Azure Prompt Flow Chat Interface")
 
     # Initialize chat history
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = []
-
-    data = {"chat_history": [], "question": "summarize the conversatio above?"}
 
     # Display chat history
     for interaction in st.session_state.chat_history:
@@ -38,7 +33,6 @@ def main():
         if interaction["outputs"]["answer"]:
             with st.chat_message("assistant"):
                 st.write(interaction["outputs"]["answer"])
-
 
     # React to user input
     if user_input := st.chat_input("Ask me anything..."):
@@ -53,32 +47,24 @@ def main():
         url = 'https://shahml-hhrub.eastus.inference.ml.azure.com/score'
         headers = {
             'Content-Type': 'application/json',
-            'Authorization': f'Bearer {AZURE_KEY}',
+            'Authorization': f'Bearer {AZURE_ENDPOINT_KEY}',
             'azureml-model-deployment': 'shahml-hhrub-1'
         }
         req = urllib.request.Request(url, body, headers)
 
         try:
             response = urllib.request.urlopen(req)
-
-            result = response.read().decode('utf-8')
-            response_data = json.loads(result)
-
-            # Extract bot response from the API response
-            bot_response = response_data['answer']
-
-            # # Add bot response to chat history
-            # chat_history[-1]["outputs"]["answer"] = bot_response
-
-            # # Update session state
-            # st.session_state.chat_history = chat_history
-
-            # Add user input to chat history
-            st.session_state.chat_history.append({"inputs": {"question": user_input}, "outputs": {"answer": bot_response}})
+            response_data = json.loads(response.read().decode('utf-8'))
 
             # render
             with st.chat_message("assistant"):
-                st.markdown(bot_response) 
+                st.markdown(response_data['answer']) 
+
+            # add user input to chat history
+            st.session_state.chat_history.append(
+                {"inputs": {"question": user_input},
+                 "outputs": {"answer": response_data['answer']}}
+            )
 
         except urllib.error.HTTPError as error:
             st.error(f"The request failed with status code: {error.code}")
